29bad32a6babf6c74a9dd73eb88bf7c436edbcb9
Min Woo Jeon <jeonminwoo2000@gmail.com>
2024-06-10 23:20:40 +0900

    update Main1.java

---
+++ d1Pr/Main1.java
@@ -0,0 +1,28 @@
+package com.fastcampus.ch3.d1Pr;
+
+import java.io.FileReader;
+import java.util.Objects;
+import java.util.Properties;
+
+class Car{}
+class SportsCar extends Car{}
+class Truck extends Car{ }
+
+class Engine {}
+
+public class Main1{
+
+    public static void main(String[] args) throws Exception{
+        Engine engine = (Engine) getObject("engine");
+        System.out.println("engine = " + engine);
+    }
+
+    static Object getObject(String key) throws Exception{
+        Properties p = new Properties();
+        p.load(new FileReader("config.txt"));
+
+        Class clazz = Class.forName(p.getProperty(key));
+        return (Engine)clazz.newInstance();
+    }
+
+}
